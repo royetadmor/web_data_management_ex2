a969156ae57e7a3ddb712cf20c1585d1189c02a3
roye <tadmoroye@gmail.com>
2021-06-26 15:46:09 +0300

    Build and save inverted index

--- inverted_index_builder.py
+++ inverted_index_builder.py
@@ -2,28 +2,55 @@ import os
 from lxml import etree
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import json
 
+
+"""
+Builds and saves the inverted index.
+The inverted index is a dictionary of the following form:
+    1. Keys - All words (excluding stop words) in our corpus
+    2. Values - Array of tuples of the form (record_id,tf-idf score)
+"""
 
 def build_inverted_index(path):
     inverted_index = {}
     xml_files = get_xml_files_data(path)
     data = extract_data_from_files(xml_files)
+    amount_of_documents = len(data)
     df = load_data_to_dataframe(data)
+
+    ### Tokenaize and count amount of words in each document ###
     cv = CountVectorizer(stop_words="english")
     cv_matrix = cv.fit_transform(df['text'])
     vocabulary = cv.get_feature_names()
     for word in vocabulary:
         inverted_index[word] = []
     df_dtm = pd.DataFrame(cv_matrix.toarray(), index=df['record_num'].values, columns=cv.get_feature_names())
+
+    ### Builds the inverted index, initially with normalized tf scores ###
     for index, row in df_dtm.iterrows():
+        max_frequency = max(row)
         for word in vocabulary:
-            if(row[word] > 0):
-                inverted_index[word].append((index,row[word]))
+            if (row[word] > 0):
+                inverted_index[word].append((index, (row[word]) / max_frequency))
+
+    ### Calculates the idf score for each word, and sets the tf-idf score for each word, per each document ###
+    for word in vocabulary:
+        idf_score = np.log2(amount_of_documents / len(inverted_index[word]))
+        new_scores = []
+        for scores in inverted_index[word]:
+            tf_idf_score = scores[1] * idf_score
+            new_scores.append((scores[0], tf_idf_score))
+        inverted_index[word] = new_scores
+
+    ### Saves the inverted index ###
+    save_index(inverted_index)
 
 
 """
 Given the loaded xml files, returns a list of dictionary, containing:
-    1. paper_num - Unique ID of each paper
+    1. record_num - Unique ID of each record
     2. title - title of each document
     3. text - A textual summary of the document. Each document either has a "abstract" or "extract" part,
               Containing the textual summary.
@@ -64,6 +91,14 @@ def get_xml_files_data(path):
     return all_files
 
 
+"""
+Loads the initial data dictionary to a dataframe.
+Indices of the dataframe are:
+    1. record_num - Unique ID of each record
+    2. text - title + summary of the paper
+"""
+
+
 def load_data_to_dataframe(data):
     paper_nums = []
     texts = []
@@ -72,6 +107,16 @@ def load_data_to_dataframe(data):
         texts.append(data[i]["title"] + data[i]["text"])
     df = pd.DataFrame({"record_num": paper_nums, "text": texts})
     return df
+
+
+"""
+Saves the inverted index to disk.
+"""
+
+
+def save_index(inverted_index):
+    with open('vsm_inverted_index.json', 'w') as outfile:
+        json.dump(inverted_index, outfile)
 
 
 build_inverted_index("document_corpus")
