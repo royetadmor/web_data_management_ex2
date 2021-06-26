49b330fb3881fb735e65e404a51a601ecde22ef9
roye <tadmoroye@gmail.com>
2021-06-26 14:31:24 +0300

    Inverted index hashMap

--- inverted_index_builder.py
+++ inverted_index_builder.py
@@ -1,10 +1,24 @@
 import os
 from lxml import etree
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def build_inverted_index(path):
+    inverted_index = {}
     xml_files = get_xml_files_data(path)
     data = extract_data_from_files(xml_files)
+    df = load_data_to_dataframe(data)
+    cv = CountVectorizer(stop_words="english")
+    cv_matrix = cv.fit_transform(df['text'])
+    vocabulary = cv.get_feature_names()
+    for word in vocabulary:
+        inverted_index[word] = []
+    df_dtm = pd.DataFrame(cv_matrix.toarray(), index=df['record_num'].values, columns=cv.get_feature_names())
+    for index, row in df_dtm.iterrows():
+        for word in vocabulary:
+            if(row[word] > 0):
+                inverted_index[word].append((index,row[word]))
 
 
 """
@@ -20,8 +34,8 @@ def extract_data_from_files(files):
     documents_identifier = []
     i = 0
     for file in files:
-        for paper_num in file.xpath("//root//RECORD//PAPERNUM//text()"):
-            documents_identifier.append({"paper_num":paper_num})
+        for record_num in file.xpath("//root//RECORD//RECORDNUM//text()"):
+            documents_identifier.append({"record_num": record_num})
     for file in files:
         for title in file.xpath("//root/RECORD//TITLE//text()"):
             documents_identifier[i]["title"] = title
@@ -48,6 +62,16 @@ def get_xml_files_data(path):
         data = etree.fromstring(xml_file.read())
         all_files.append(data)
     return all_files
+
+
+def load_data_to_dataframe(data):
+    paper_nums = []
+    texts = []
+    for i in range(len(data)):
+        paper_nums.append(data[i]["record_num"])
+        texts.append(data[i]["title"] + data[i]["text"])
+    df = pd.DataFrame({"record_num": paper_nums, "text": texts})
+    return df
 
 
 build_inverted_index("document_corpus")
